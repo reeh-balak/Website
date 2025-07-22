f2629cd008863f2090796e591c52a4e43d0ec413
ahmad1212as <157467380+ahmad1212as@users.noreply.github.com>
2025-07-22 19:31:23 +0300

    Create script.js

---
+++ Download the app/script.js
@@ -0,0 +1,27 @@
+function downloadFile() {
+  const url = "https://reeh-balak.github.io/Website/Desktop%20application/Reeh%20Balak%20Setup.exe";
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = '';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+// منع النقر بالزر الأيمن
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
+
+// منع اختصارات أدوات المطور
+document.addEventListener('keydown', function(e) {
+  if (e.key === "F12") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && ['i', 'j', 'c'].includes(e.key.toLowerCase())) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && ['u', 's'].includes(e.key.toLowerCase())) {
+    e.preventDefault();
+  }
+});
